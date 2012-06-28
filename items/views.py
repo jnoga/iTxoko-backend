6fc93a22c35de021b47b0b065293f45a2eeb110f
dlacroixe <lukatoni9@gmail.com>
2012-06-28 11:42:40 +0200

    cambios para iwalar

--- items/views.py
+++ items/views.py
@@ -1,18 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 import utils
-<<<<<<< HEAD
-import urllib
-import urllib2
-import cookielib
-import re
-import mechanize
-from items.models import Item
-=======
 from items.models import Account
->>>>>>> 8bcb2dc31b3989c2e997d23f35bd2ab6847c6c9e
-
-
 
 def index(request):
 	try:
@@ -39,12 +28,8 @@ def index(request):
 
 def updateRss(request) :
 	try:
-<<<<<<< HEAD
-		utils.parseRSSEntries('http://www.meneame.net/rss2.php?meta=0')
-=======
 		for ac in Account.objects.filter(active=True, account_type='rss'):
 			utils.parseRSSEntries(ac.server_url)
->>>>>>> 8bcb2dc31b3989c2e997d23f35bd2ab6847c6c9e
 	except Exception as ex:
 		return HttpResponse("Exception: %s" %ex)
 	
@@ -88,16 +73,10 @@ def updateTT(request):
 	"""
 
 	try:
-<<<<<<< HEAD
-		links = utils.parseCoursesLink('http://campus.eps.mondragon.edu/pls/hor/wwwHor.Lista?pPer=11218&pObj=C&pTipo=2&pCols=1')
-		for link in links:
-			utils.parseTimeTable(link)
-=======
 		for ac in Account.objects.filter(active=True, account_type='tt'):
 			links = utils.parseCoursesLink(ac.server_url)
 			for link in links:
 				utils.parseTimeTable(link)
->>>>>>> 8bcb2dc31b3989c2e997d23f35bd2ab6847c6c9e
 	except Exception, e:
 		return HttpResponse("Exception: %s" %e)
 
@@ -105,16 +84,8 @@ def updateTT(request):
 
 def updateMail(request):
 	try:
-<<<<<<< HEAD
-		utils.parsePOPMail('pop.1and1.es', 'jnogales@gukere.com', '$jn0gal3s', 'contacto@gukere.com')
-	except Exception, e:
-		return HttpResponse("Exception: %s" %e)
-	return HttpResponse("updateMail")
-
-=======
 		for ac in Account.objects.filter(active=True, account_type='mail'):
 			utils.parsePOPMail(ac.server_url, ac.username, ac.password, ac.content_filter)
 	except Exception, e:
 		return HttpResponse("Exception: %s" %e)
 	return HttpResponse("updateMail")
->>>>>>> 8bcb2dc31b3989c2e997d23f35bd2ab6847c6c9e
